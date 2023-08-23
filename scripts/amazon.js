3daa519b81ef14bbc241b04793c24896dcee3e32
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-08-23 23:52:30 +0200

    Adding the Search Bar and Hints to Product Names

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,13 +8,13 @@ let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
-    <div class="product-container">
+    <div class="product-container" data-product-container-${product.id}>
     <div class="product-image-container">
     <img class="product-image"
     src="${product.image}">
     </div>
     
-    <div class="product-name limit-text-to-2-lines">
+    <div class="product-name limit-text-to-2-lines" data-product-name-${product.id}>
     ${product.name}
     </div>
     
@@ -96,3 +96,68 @@ document.querySelectorAll('.js-add-to-cart').forEach((btn) => {
     }, 2000); 
   });
 });
+
+
+
+// * search bar
+
+function findBySearch(value) {
+  // * cards show by the input text  
+  let coincidenceCount = 0;
+  
+  products.forEach(product => {
+    const productName = product.name.toLowerCase();
+    const productKeywords = product.keywords;
+
+
+    let isVisible = productName.includes(value);
+
+
+    if (!isVisible) {
+      productKeywords.forEach(keyword => {
+        if (keyword === value) {
+          isVisible = true;
+          coincidenceCount += 1;
+        }
+      });
+    }
+    
+
+
+    const productContainer = document.querySelector(`[data-product-container-${product.id}]`);
+    
+    productContainer.classList.toggle('hide', !isVisible);
+    
+    // * highlight the input text
+    
+    const cardName = document.querySelector(`[data-product-name-${product.id}]`);
+    
+    cardName.innerHTML = highlightMatches(productName, value);
+      
+    });
+}
+
+
+function highlightMatches(text, searchValue) {
+  const re = new RegExp(searchValue, 'gi');
+  return text.replace(re, match => `<span class="highlight">${match}</span>`);
+}
+
+
+function searchBtnOnClick() {
+  const searchInput = document.querySelector('[data-search-input]').value;
+  findBySearch(searchInput);
+}
+
+
+document.body.addEventListener('keydown', event => {
+  if (event.key === "Enter") {
+    searchBtnOnClick();
+  }
+});
+
+const searchBtn = document.querySelector('[data-search-button]');
+searchBtn.addEventListener('click', searchBtnOnClick);
+
+
+searchBtnOnClick();
