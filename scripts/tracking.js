798f7cfa3d7e187ae2378aeed4fb0a3edd226ee9
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-09-02 22:07:20 +0200

    Implementing new design for tracking page

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -4,10 +4,11 @@ import { getDate } from './utils/time.js';
 
 let currentProduct;
 
-const deliveryDateWithDashes = productData.deliveryDate.replace(/\//g, '-');
-const preparingDateWithDashes = productData.preparingDate.replace(/\//g, '-');
-console.log(deliveryDateWithDashes)
-console.log(preparingDateWithDashes)
+
+const preparingDateComponents = productData.preparingDate.split('/');
+const deliveryDateComponents = productData.deliveryDate.split('/');
+const currentDateComponents = getDate().split('/');
+
 
 
 products.forEach(product => {
@@ -59,3 +60,60 @@ const innerHTML = `
 
 const selecorDiv = document.querySelector('.main');
 selecorDiv.innerHTML = innerHTML;
+
+
+
+// * brogress bar
+
+
+let preparingDate = new Date(
+    parseInt(preparingDateComponents[2]),
+    parseInt(preparingDateComponents[1]),
+    parseInt(preparingDateComponents[0]) 
+  );
+
+let deliveryDate = new Date(
+    parseInt(deliveryDateComponents[2]),
+    parseInt(deliveryDateComponents[1]),
+    parseInt(deliveryDateComponents[0]) 
+  );
+
+let currentDate = new Date(
+    parseInt(currentDateComponents[2]),
+    parseInt(currentDateComponents[1]),
+    parseInt(currentDateComponents[0]) 
+  );
+
+preparingDate = preparingDate.toDateString();
+deliveryDate = deliveryDate.toDateString();
+currentDate = currentDate.toDateString();
+
+
+console.log("preparing date " + preparingDate);
+console.log("delivery date " + deliveryDate);
+console.log("current date " + currentDate);
+
+
+
+
+const progressBar = document.querySelector('.progress-bar');
+
+
+if (currentDate >= deliveryDate) {
+    progressBar.classList.add('progress-bar-delivered');
+
+    console.log('delivered');
+}
+else if (currentDate === preparingDate) {
+    progressBar.classList.add('progress-bar-preparing');
+    
+    console.log('preparing');
+}
+else if (currentDate < deliveryDate || currentDate > preparingDate) {
+    progressBar.classList.add('progress-bar-shipped');
+
+    console.log('shipped');
+}
+else {
+    console.log('nothing');
+}
